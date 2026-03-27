f6748f5c205ba6e3d1a9e4f9bdc136a88bb7239a
Ritu Swami <yourgithubemail@gmail.com>
2026-03-27 22:27:54 +0530

    fixed port and db

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const userRouter = require("./routers/user.router");
 const server = express();
 
 const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 const HOST = process.env.HOST || "localhost";
 
 server.use(cors());
